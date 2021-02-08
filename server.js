f9e2efc8c8d6032f49b8a4d2fbe17f9fea1df6ba
Travis Ko <tkospeaks@gmail.com>
2021-02-07 23:09:43 -0800

    Adding in server.js for port connection

---
+++ server.js
@@ -0,0 +1,33 @@
+// Create requires for express, mongooseDB, and logging (logger) //
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// This will automatically run the seed for schema //
+! require("./seeders/seed");
+
+// Start app at port 8080 //
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Mongoose connect process //
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// Requiring HTML and API routes for routing //
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+
+// App listening on selected Port //
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
